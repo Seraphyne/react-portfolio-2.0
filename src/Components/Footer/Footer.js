718781be161e8ef39ac244be0fce977dc8dc7b4a
Seraphyne <renatafd717@gmail.com>
2020-11-11 23:52:35 -0600

    fixed errors on WAVE links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,12 +9,12 @@ export default function Footer(props) {
     <footer>
       <div className="footer">
         <ul className="social-links row">
-          <li><a href="https://github.com/Seraphyne" rel="noopener noreferrer" target="_blank"><i className="fa fa-github" /></a></li>
-          <li><a href="https://www.linkedin.com/in/renatafd/?locale=en_US" rel="noopener noreferrer" target="_blank"><i className="fa fa-linkedin" /></a></li>
-          <li><a href="https://stackoverflow.com/users/14376189/renata-dickinson" rel="noopener noreferrer" target="_blank"><i className="fa fa-stack-overflow" /></a></li>
-          <li><a href="https://www.hackerrank.com/renata_dickinson" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={faHackerrank} /></a></li>
-          <li><a href="https://devpost.com/seraphyne" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={faDev} /></a></li>
-          <li><a href="mailto: renata.f.dickinson@gmail.com?subject=I saw your Portfolio." rel="noopener noreferrer" target="_blank"><i className="fa fa-envelope" /></a></li>
+          <li><a href="https://github.com/Seraphyne" rel="noopener noreferrer" target="_blank" aria-label="github"><i className="fa fa-github" /></a></li>
+          <li><a href="https://www.linkedin.com/in/renatafd/?locale=en_US" rel="noopener noreferrer" target="_blank" aria-label="linkedin"><i className="fa fa-linkedin" /></a></li>
+          <li><a href="https://stackoverflow.com/users/14376189/renata-dickinson" rel="noopener noreferrer" target="_blank" aria-label="stackoverflow"><i className="fa fa-stack-overflow" /></a></li>
+          <li><a href="https://www.hackerrank.com/renata_dickinson" rel="noopener noreferrer" target="_blank" aria-label="hackerrank"><FontAwesomeIcon icon={faHackerrank} /></a></li>
+          <li><a href="https://devpost.com/seraphyne" rel="noopener noreferrer" target="_blank" aria-label="devpost"><FontAwesomeIcon icon={faDev} /></a></li>
+          <li><a href="mailto: renata.f.dickinson@gmail.com?subject=I saw your Portfolio." rel="noopener noreferrer" target="_blank" aria-label="mail"><i className="fa fa-envelope" /></a></li>
         </ul>
       </div>
     </footer>
