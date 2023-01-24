79e1541c12c23b0f60e15c235659614c284efc15
RD <renatafd717@gmail.com>
2023-01-24 01:20:08 -0600

    updating some techs

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,50 +1,51 @@
-import React from 'react';
-import './Skills.css';
+import React from "react";
+import "./Skills.css";
 
 export default function Skills() {
-    return (
-      <section id="skills">
-        <div className="main-row">
-          <div className="column main-col flex">
-            <h2>Languages</h2>
-            <div className="column main-col">
-              <h4>FrontEnd</h4>
-              <li>HTML5</li>
-              <li>JavaScript</li>
-              <li>CSS</li>
-              <li>Sass</li>
-              <li>jQuery</li>
-              <li>React</li>
-              <li>React Router</li>
-              <li>Ruby</li>
-              <li>Haml</li>
-              <li>Enzyme</li>
-              <li>Cypress.io</li>
-            </div>
-            <div className="column main-col">
-              <h4>BackEnd</h4>
-              <li>PostgreSQL</li>
-              <li>Node.js</li>
-              <li>Express</li>
-              <li>JWT - JSON Web Token</li>
-              <li>Ruby on Rails</li>
-              <li>Mocha & Chai</li>
-              <li>Nodemon</li>
-              <li>Supertest</li>
-            </div>
-            <div className="column main-col">
-              <h4>DevTools</h4>
-              <li>Git</li>
-              <li>GitHub</li>
-              <li>Chrome DevTools</li>
-              <li>Jester</li>
-              <li>Travis Ci</li>
-              <li>Heroku</li>
-              <li>Vercel</li>
-              <li>Netlify</li>
-            </div>
+  return (
+    <section id="skills">
+      <div className="main-row">
+        <div className="column main-col flex">
+          <h2>Languages</h2>
+          <div className="column main-col">
+            <h4>FrontEnd</h4>
+            <li>HTML5</li>
+            <li>JavaScript</li>
+            <li>CSS</li>
+            <li>Sass</li>
+            <li>jQuery</li>
+            <li>React</li>
+            <li>React Router</li>
+            <li>Ruby</li>
+            <li>Haml</li>
+            <li>Enzyme</li>
+            <li>Cypress.io</li>
+          </div>
+          <div className="column main-col">
+            <h4>BackEnd</h4>
+            <li>PostgreSQL</li>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>JWT - JSON Web Token</li>
+            <li>Ruby on Rails</li>
+            <li>Mocha & Chai</li>
+            <li>Nodemon</li>
+            <li>Supertest</li>
+          </div>
+          <div className="column main-col">
+            <h4>DevTools</h4>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>Chrome DevTools</li>
+            <li>Jester</li>
+            <li>Travis Ci</li>
+            <li>Heroku</li>
+            <li>Railway</li>
+            <li>Vercel</li>
+            <li>Netlify</li>
           </div>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 }
