94adaa50e64c0f6faa46882506c654addcc4bf0f
RD <renatafd717@gmail.com>
2024-01-30 20:37:04 -0600

    necessary updates on site

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -12,21 +12,34 @@ export default function About(props) {
           <h2>About Me</h2>
           <p>
             Hailing all the way from <em>Belo Horizonte, Brazil</em>, I have
-            carved out my nook of the world in Dallas-Fort Worth Metroplex. I am
-            a Project Manager, with knowledge in full stack developement{" "}
-            (JavaScript, CSS, HTML, React, Git, GitHub, Node.js, PostgreSQL and
-            JWT) . I have completed the full-time Engineering Immersion program
-            at <em>Thinkful</em>, where I built a strong foundation in
-            responsive web design and full stack web development.
+            carved out my nook of the world in Dallas-Fort Worth Metroplex.
+            Having completed the full-time Engineering Immersion program at{" "}
+            <em>Thinkful</em>, I have honed my skills in full stack development,
+            encompassing JavaScript, CSS, HTML, React, Git, GitHub, Node.js,
+            PostgreSQL, and JWT. This comprehensive training has equipped me
+            with a strong foundation in responsive web design and full stack web
+            development.
           </p>
           <p>
-            In addition to my experience at <em>Thinkful</em>, my professional
-            experience ranges from tour guide, English and History teacher,
-            customer support and barista.{" "}
+            In addition to my professional journey, I have decided to embark on
+            a new chapter and transition into a different role as a{" "}
+            <em>Project Manager</em> completing the course from <em>Google</em>.
+            Navigating this change, I bring a wealth of skills, adaptability,
+            and a passion for learning.
           </p>
           <p>
-            On my downtime, I enjoy playing games, biking, reading History or
-            Biography books, watching Netflix and listening to music.{" "}
+            Outside of the professional realm, my downtime is filled with joyous
+            activities. I am an avid gamer, a biking enthusiast, and an ardent
+            reader of History and Biography books. I also find solace in
+            unwinding with Netflix and immersing myself in diverse musical
+            genres.
+          </p>
+          <p>
+            If my versatile skill set and evolving career path align with what
+            you are looking for, I welcome you to connect with me on{" "}
+            <em>LinkedIn</em> or on use the Contact page. I am excited about the
+            prospect of contributing my unique blend of experiences and skills
+            to a dynamic team.
           </p>
           <div className="row">
             <div className="column contact-details">
